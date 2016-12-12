6a712f6bdacc0dc91a083d08bf60e98470ddb04f
罗凡 <luofan@MacBook-Pro.local>
2016-12-12 19:56:50 +0800

    reset all

--- app/main.js
+++ app/main.js
@@ -1,8 +1,25 @@
-'use strict'
-var component = require('./component.js');
-var aBtn = document.getElementById('aClick');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-aBtn.addEventListener('click', function(event){
-	alert(event.type);
-	
-}, false)
+class TestComponent extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      arr: [23, 324, "tom"]
+    };
+ }
+
+  render() {
+    var {arr} = this.state;
+
+    return (<div>
+      {arr.map(function(value, index){
+        return (<p>{value} - {index}</p>)                               
+      }}    
+    </div>)
+  }
+
+} 
+
+ReactDOM.render(<TestComponent/>, document.getElementById("container"));
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 	module: {
 		loaders: [{
 			test: /\.js$/,
-			loaders: ['babel?presets[]=es2015'],
+			loaders: ['babel'],
 			exclude: /node_modules/
 		}]
 	},
